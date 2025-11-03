8f2faeaec6c6bba65bff6c8c9ddf110e31235ed0
Joanna Murawska <jkmurek@gmail.com>
2025-11-03 17:11:29 +0100

    Display users from BE

--- phonebookfe/src/App.tsx
+++ phonebookfe/src/App.tsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
+interface PhoneNumber {
+  id: number;
+  phoneNumber: string;
+}
+
 interface Person {
-  id: number
-  name: string
-  number: string
+  id: number;
+  userName: string;
+  phoneNumbers: PhoneNumber[];
 }
 
 function App() {
@@ -16,7 +21,7 @@ function App() {
   useEffect(() => {
     // Fetch phonebook data from backend
     axios
-      .get<Person[]>('http://localhost:3001/api/persons')
+      .get<Person[]>('http://localhost:8080/api/users')
       .then(response => {
         setPersons(response.data)
         setLoading(false)
@@ -36,16 +41,23 @@ function App() {
     return <div>Error: {error}</div>
   }
 
+console.log(persons)
+
   return (
     <div>
       <h1>Phonebook</h1>
       <h2>Numbers</h2>
       <div>
         {persons.map(person => (
-          <div key={person.id}>
-            {person.name} {person.number}
-          </div>
-        ))}
+        <div key={person.id}>
+          <h3>{person.userName}</h3>
+          <ul>
+            {person.phoneNumbers.map(phone => (
+              <li key={phone.id}>{phone.phoneNumber}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
       </div>
     </div>
   )
